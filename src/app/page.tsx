cde10dc44c1035e6e34e5848809f7a558ed31508
m1rage <miragela.code@gmail.com>
2025-06-16 12:17:57 +0200

    Add ASCII art avatar to the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
 import Image from "next/image";
 import Header from "../components/Header";
+import avatar_ascii from "../avatar";
 export default function Home() {
   return (
-    <div className="bg-[#171a1e] grid grid-cols-[0.1fr_1fr_0.1fr] items-center justify-items-center h-screen">
+    <div className="bg-[#000000] grid grid-cols-[0.1fr_1fr_0.1fr] items-center justify-items-center h-screen">
       <main className="col-start-2 h-full w-full">
         <Header />
+        <code>
+
+        </code>
+
+        <div className="h-full w-full flex flex-col items-center justify-center">
+          <pre className="
+          text-[#5b8075] select-none absolute top-0 left-50% text-[6px]
+          font-mono whitespace-pre leading-[1.3]">
+            {avatar_ascii}
+          </pre>
+        </div>
       </main>
     </div>
   );
 }
+
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Header() {
     return (
-        <header className="pt-5 flex justify-between h-auto w-full">
-            <div className="text-header font-bold cursor-pointer"><a href="#">m1rageLA</a></div>
-            <ul className="flex gap-10 text-li font-light cursor-pointer">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Live Demo</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Contact</a></li>
+        <header className="pt-6 flex justify-between h-auto w-full">
+            <div className="text-header text-lg font-bold cursor-pointer "><a href="#">m1rageLA</a></div>
+            <ul className="flex gap-10 text-li font-light cursor-pointer ">
+                {["Home", "About", "Projects", "Skills", "Contacts"].map((item) => {
+                    return (
+                        <li key={item} className="hover:text-li-hover cursor-pointer">{item}</li>
+                    )
+                })}
             </ul>
         </header>
     )
