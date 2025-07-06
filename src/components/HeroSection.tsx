79955b66eb5176a1f8a1439a596ec44f0404cff6
m1rage <miragela.code@gmail.com>
2025-07-06 16:44:37 +0200

    Add pictures to portfolio

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,4 +1,12 @@
 export default function HeroSection() {
+  const socials = [
+    { href: 'https://t.me/tmrrozh', src: '/telegram.png', alt: 'Telegram' },
+    { href: 'https://www.linkedin.com/in/tymur-rozhkovskyi-3a4874358/', src: './linkedin.png', alt: 'LinkedIn' },
+    { href: 'https://github.com/m1rageLA?tab=repositories', src: '/github.png', alt: 'GitHub' },
+    { href: 'https://www.behance.net/mirage_LA', src: '/behance.png', alt: 'Behance' },
+    { href: 'https://www.fiverr.com/s/pdXz97Z', src: '/fiverr.png', alt: 'Fiverr' },
+  ];
+
   return (
     <section className="relative z-20 max-w-[45ch]">
       <h1 className="text-lg font-bold tracking-[0.05em]">I'm Tymur&nbsp;Rozhkovskyi</h1>
@@ -9,16 +17,19 @@ export default function HeroSection() {
         voluptatum blanditiis maxime consectetur, sunt quisquam quam
         molestias voluptas eligendi repellat vero at saepe.
       </p>
-      <div className="relative flex w-full h-6 gap-3 mt-6 group">
-        <a href="https://t.me/tmrrozh" className="transition-opacity duration-300 opacity-60 hover:opacity-100 group-hover:opacity-30">
-          <img className="w-full h-full" src="./telegram.png" alt="Telegram" />
-        </a>
-        <a href="https://www.linkedin.com/in/tymur-rozhkovskyi-3a4874358/" className="transition-opacity duration-300 opacity-60 hover:opacity-100 group-hover:opacity-30">
-          <img className="w-full h-full" src="./linkedin.png" alt="LinkedIn" />
-        </a>
-        <a href="https://github.com/m1rageLA" className="transition-opacity duration-300 opacity-60 hover:opacity-100 group-hover:opacity-30">
-          <img className="w-full h-full" src="./github.png" alt="GitHub" />
-        </a>
+      <div className="mt-8 flex gap-3 group">
+        {socials.map(({ href, src, alt }) => (
+          <a
+            key={alt}
+            href={href}
+            aria-label={alt}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-opacity duration-300 opacity-60 hover:opacity-100 group-hover:opacity-30"
+          >
+            <img className="w-8 h-8 md:w-6 md:h-6" src={src} alt={alt} />
+          </a>
+        ))}
       </div>
 
     </section>
